8db0d110e94ff03014279ec08425a9f47396d8d2
Cindy Michalowski <chiwhitesox56@gmail.com>
2021-01-13 22:10:07 -0500

    Tried using material UI; button and test not nested in common cell

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import './App.css';
 import { getFlavors } from './data/flavors';
-import { Card, Image, Row, Col } from "react-bootstrap"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ButtonBase, Grid } from '@material-ui/core';
+
 
 const flavors = getFlavors()
 
-function ListItem({flavor}) {
+function FlavorCard({flavor}) {
   return (
-    <Card border="info">
-      <Card.Body>
-        <Card.Text><h2>{flavor.flavor}</h2></Card.Text>
-        <Card.Img
-          className="img"
-          variant="right"
-          aria-label={flavor.flavor}
-          src={flavor.image}
-          alt={`An image of ${flavor.flavor} ice`}
-          />
-        </Card.Body>
-    </Card>
+    <>
+    <Grid container className="myGrid" justify="center">
+      <Grid item className="griddyImageColumn" >
+        <ButtonBase className="myGridButton">
+            <img aria-label={flavor.flavor} src={flavor.image} />
+          </ButtonBase>
+          </Grid>
+      <Grid item className="myGridTextColumn" xs={12} sm container>
+      <h2>{flavor.flavor}</h2>
+      {/* </Grid>
+      <Grid item className="griddyFlavorImage"> */}
+      {/* <img
+        aria-label={flavor.flavor}
+        src={flavor.image} */}
+        {/* //alt={`An image of ${flavor.flavor} ice`}
+        /> */}
+      </Grid>
+    </Grid>
+    </>
   );
 };
+
+// function FlavorCard({flavor}) {
+//   return (
+//     <Card border="info">
+//       <Card.Body>
+//         <Card.Text><h2>{flavor.flavor}</h2></Card.Text>
+//         <Card.Img
+//           className="img"
+//           variant="right"
+//           aria-label={flavor.flavor}
+//           src={flavor.image}
+//           alt={`An image of ${flavor.flavor} ice`}
+//           />
+//         </Card.Body>
+//     </Card>
+//   );
+// };
 
 function App() {
 
   return (
-    <div className="list">{flavors.map((item) => <ListItem flavor={item} key={item.flavor}/> )}</div>
+    <div className="list">{flavors.map((item) => <FlavorCard flavor={item} key={item.flavor}/> )}</div>
   );
 }
 
